b3949e4825181a5559809a8d966e12ca3a2f0212
Boyce <Aaron.Ngen@outlook.com>
2018-03-27 22:13:05 +0800

    A simple Shiro demo with Spring Boot.

--- src/main/java/org/boyce/repository/UserInfoRepository.java
+++ src/main/java/org/boyce/repository/UserInfoRepository.java
@@ -1,9 +1,9 @@
-package com.neo.dao;
+package org.boyce.repository;
 
-import com.neo.entity.UserInfo;
+import org.boyce.entity.UserInfo;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserInfoDao extends CrudRepository<UserInfo,Long> {
+public interface UserInfoRepository extends CrudRepository<UserInfo,Long> {
     /**通过username查找用户信息;*/
     public UserInfo findByUsername(String username);
 }
