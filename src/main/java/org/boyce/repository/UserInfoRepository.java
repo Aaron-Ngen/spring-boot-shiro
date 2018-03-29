cd184d98656ae7715ea8c1cd1ebb426826894aad
Boyce <Aaron.Ngen@outlook.com>
2018-03-29 23:28:41 +0800

    spring-boot-shiro

--- src/main/java/org/boyce/repository/UserInfoRepository.java
+++ src/main/java/org/boyce/repository/UserInfoRepository.java
@@ -4,6 +4,7 @@ import org.boyce.entity.UserInfo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserInfoRepository extends CrudRepository<UserInfo,Long> {
+
     /**通过username查找用户信息;*/
-    public UserInfo findByUsername(String username);
+     UserInfo findByUsername(String username);
 }
